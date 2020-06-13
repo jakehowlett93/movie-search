4a0394e052290b4a75c39bb43714d797f8c68095
Jake Howlett <41288146+jakehowlett93@users.noreply.github.com>
2020-06-13 12:11:47 +0100

    readying the site for deployment

---
+++ build/precache-manifest.0a01c471c58c65acd754f410739d63c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39b08d908e57848fa58db44ea754496",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a4a2d538448ee9b85c2",
+    "url": "/static/css/main.056407e7.chunk.css"
+  },
+  {
+    "revision": "f1347bdf9c36244625c7",
+    "url": "/static/js/2.e97a99ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e97a99ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4a2d538448ee9b85c2",
+    "url": "/static/js/main.b83b16bd.chunk.js"
+  },
+  {
+    "revision": "56610139b6d40ca7cbf2",
+    "url": "/static/js/runtime-main.6dcec938.js"
+  }
+]);
