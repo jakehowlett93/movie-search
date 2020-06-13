e69c65113fd3289388f91c82b6b43fa516197b45
Jake Howlett <41288146+jakehowlett93@users.noreply.github.com>
2020-06-13 18:18:59 +0100

    added a default image to display when the poster is N/A

--- src/components/movieDetail/MovieDetail.js
+++ src/components/movieDetail/MovieDetail.js
@@ -10,17 +10,22 @@ const MovieDetail = ({ fetchedDetails, error }) => {
     } else if (error) {
         return <div>{error}</div>
     }
+
+    const ratings = fetchedDetails.Ratings === true ? fetchedDetails.Ratings[0].Value : 'N/A';
+    const noImage = require('../../assets/noImage.png')
+    const poster = fetchedDetails.Poster === 'N/A' ? noImage : fetchedDetails.Poster;    
+    const altText = fetchedDetails.Poster === 'N/A' ? 'Could not find movie poster' : 'A poster of the movie'
     
     return (
         <div className="details">
             <h1 className="details-title">{fetchedDetails.Title} </h1>
             <div className="images">
-                <img className="details-image" src={fetchedDetails.Poster} alt='The selected movie poster'></img>
-                <img className="blurred-image" src={fetchedDetails.Poster} alt='The selected movie poster'></img>
+                <img className="details-image" src={poster} alt={altText}></img>
+                <img className="blurred-image" src={poster} alt={altText}></img>
             </div>
             <p className="details-paragraph">
                 <br />
-                <span className="bold-text">Rating: </span>{fetchedDetails.Ratings[0].Value}
+                <span className="bold-text">Rating: </span>{ratings}
                 <br />
                 <span className="bold-text">Release: </span>{fetchedDetails.Released}
                 <br />
