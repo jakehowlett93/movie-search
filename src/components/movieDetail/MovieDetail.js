601cb406e485b2587e3c0fc1060a9cffdbc06fa8
Jake Howlett <41288146+jakehowlett93@users.noreply.github.com>
2020-06-14 12:52:12 +0100

    Revert "Revert "Revert "converted movieDetail to a class based function"""
    This reverts commit 20d03f6b0cd9822cba8e16fa29f8688cc2295e43.

--- src/components/movieDetail/MovieDetail.js
+++ src/components/movieDetail/MovieDetail.js
@@ -4,43 +4,39 @@ import './MovieDetail.css';
 
 
 
-class MovieDetail extends React.Component {
-
-    render() {
-        if (!this.props.fetchedDetails && !this.props.error) {
-            return <div>Search the Open Movie Database</div>
-        } else if (this.props.error) {
-            return <div>{this.props.error}</div>
-        } else {
-            const fetchedDetails = this.props.fetchedDetails
-            const rating = fetchedDetails.Ratings === true ? fetchedDetails.Ratings[0].Value : 'N/A';
-            const noImage = require('../../assets/noImage.png')
-            const poster = fetchedDetails.Poster === 'N/A' ? noImage : fetchedDetails.Poster;    
-            const altText = fetchedDetails.Poster === 'N/A' ? 'Could not find movie poster' : 'A poster of the movie'
-            return (
-                <div className="details">
-                    <h1 className="details-title">{this.props.fetchedDetails.Title} </h1>
-                    <div className="images">
-                        <img className="details-image" src={poster} alt={altText}></img>
-                        <img className="blurred-image" src={poster} alt={altText}></img>
-                    </div>
-                    <p className="details-paragraph">
-                        <br />
-                        <span className="bold-text">Rating: </span>{rating}
-                        <br />
-                        <span className="bold-text">Release: </span>{fetchedDetails.Released}
-                        <br />
-                        <span className="bold-text">Runtime: </span>{fetchedDetails.Runtime}
-                        <br />
-                        <span className="bold-text">Cast: </span>{fetchedDetails.Actors}
-                        <br />
-                        <span className="bold-text">Plot: </span>{fetchedDetails.Plot}
-                    </p>
-                </div>
-            )
-        }
+const MovieDetail = ({ fetchedDetails, error }) => {
+    if (!fetchedDetails && !error) {
+        return <div>Search the Open Movie Database</div>
+    } else if (error) {
+        return <div>{error}</div>
     }
+
+    const ratings = fetchedDetails.Ratings === true ? fetchedDetails.Ratings[0].Value : 'N/A';
+    const noImage = require('../../assets/noImage.png')
+    const poster = fetchedDetails.Poster === 'N/A' ? noImage : fetchedDetails.Poster;    
+    const altText = fetchedDetails.Poster === 'N/A' ? 'Could not find movie poster' : 'A poster of the movie'
     
+    return (
+        <div className="details">
+            <h1 className="details-title">{fetchedDetails.Title} </h1>
+            <div className="images">
+                <img className="details-image" src={poster} alt={altText}></img>
+                <img className="blurred-image" src={poster} alt={altText}></img>
+            </div>
+            <p className="details-paragraph">
+                <br />
+                <span className="bold-text">Rating: </span>{ratings}
+                <br />
+                <span className="bold-text">Release: </span>{fetchedDetails.Released}
+                <br />
+                <span className="bold-text">Runtime: </span>{fetchedDetails.Runtime}
+                <br />
+                <span className="bold-text">Cast: </span>{fetchedDetails.Actors}
+                <br />
+                <span className="bold-text">Plot: </span>{fetchedDetails.Plot}
+            </p>
+        </div>
+    )
 };
 
 const mapStateToProps = (state) => {
